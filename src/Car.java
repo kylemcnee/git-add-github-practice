e7262ceaeb9ec2dbee61bc9ada9a565d918b8d8e
kmcnee <kyle.mcnee@gmail.com>
2016-01-14 12:32:01 -0500

    Added member variables, added Garage class

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,7 @@
  * Created by perrycooperman on 1/14/16.
  */
 public class Car {
+    int mSpeed;
+    double mMileage;
+    String mModel;
 }
---
+++ src/Garage.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Kyle McNee on 1/14/2016.
+ */
+public class Garage {
+    
+}
